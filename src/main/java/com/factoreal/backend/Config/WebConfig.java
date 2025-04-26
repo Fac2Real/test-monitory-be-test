90c15f70e9ba26f320213f2aa817d4fc077b3497
JuneYoo <juneyo0823@naver.com>
2025-04-26 10:50:26 +0900

    WIP: 임시 커밋

---
+++ src/main/java/com/factoreal/backend/Config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.factoreal.backend.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // 혹은 "/**" 전체 허용
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true); // 필요한 경우에만
+            }
+        };
+    }
+}
--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO sensor(sensor_id, sensor_type, sensor_status) VALUES 
-  ('S001', 'Temperature', 'ACTIVE'),
-  ('S002', 'Humidity',    'INACTIVE'),
-  ('S003', 'Light',       'ACTIVE'),
-  ('S004', 'Sound',       'INACTIVE'),
-  ('S005', 'Motion',      'ACTIVE');
