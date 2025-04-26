13422dd956cf4b3ce0db5c07ff55fe60bbee80d4
JuneYoo <juneyo0823@naver.com>
2025-04-26 11:32:37 +0900

    docs | 없음 | 없음 |swagger docs 적용| 유승희

--- src/main/java/com/factoreal/backend/BackendApplication.java
+++ src/main/java/com/factoreal/backend/BackendApplication.java
@@ -1,8 +1,17 @@
 package com.factoreal.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Factoreal Swagger API",
+				description = "Factoreal BE Swagger API",
+				version = "v1.0.0"
+		)
+)
 @SpringBootApplication
 public class BackendApplication {
 
--- src/main/java/com/factoreal/backend/Controller/SensorController.java
+++ src/main/java/com/factoreal/backend/Controller/SensorController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Map;
 
 import com.factoreal.backend.Entity.Sensor;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import com.factoreal.backend.Service.SensorService;
 
 @RestController
 @RequestMapping("/api/sensors")
+@Tag(name = "센서 정보 API", description = "센서 정보 처리 API입니다.")
 public class SensorController {
 
     private final SensorService service;
@@ -25,6 +28,7 @@ public class SensorController {
 
     // 센서 등록 (임시 코드)
     @PostMapping
+    @Operation(summary = "센서 등록 (임시 기능)", description = "센서 정보를 수동으로 등록하는 임시 메서드", hidden = true)
     public ResponseEntity<Map<String, Boolean>> createSensor(@RequestBody SensorDto dto) {
         Map<String, Boolean> response = new HashMap<>();
         try {
@@ -39,23 +43,26 @@ public class SensorController {
 
     // 전체 센서 리스트 조회 ( BE -> FE 센서ID, 센서종류 넘기기 )
     @GetMapping
+    @Operation(summary = "전체 센서 리스트 조회", description = "전체 센서 정보를 조회하는 기능")
     public ResponseEntity<List<SensorDto>> list() {
         return ResponseEntity.ok(service.getAllSensors());
     }
 
+    // 미등록 센서 리스트 조회 ( BE -> FE )
+    @GetMapping("/unregistered")
+    @Operation(summary = "미등록 센서 리스트 조회", description = "미등록 센서 데이터 조회 (BE -> FE)")
+    public ResponseEntity<List<SensorDto>> getUnregisteredSensors() {
+        List<SensorDto> sensors = service.getUnregisteredSensors();
+        return ResponseEntity.ok(sensors);
+    }
+
     // DB Sensor Table 업데이트 ( FE -> BE 센서ID 매핑해서 센서목적, 위치, 임계치 업데이트 )
     @PostMapping("/{sensorId}")
+    @Operation(summary = "센서 정보 업데이트", description = "센서ID 매핑해서 센서목적, 위치, 임계치 업데이트 (FE -> BE) ")
     public ResponseEntity<Void> update(
             @PathVariable("sensorId") String sensorId,
             @RequestBody SensorUpdateDto dto) {
         service.updateSensor(sensorId, dto);
         return ResponseEntity.noContent().build();
-    }
-
-    // 미등록 센서 리스트 조회 ( BE -> FE )
-    @GetMapping("/unregistered")
-    public ResponseEntity<List<SensorDto>> getUnregisteredSensors() {
-        List<SensorDto> sensors = service.getUnregisteredSensors();
-        return ResponseEntity.ok(sensors);
     }
 }
