5ec72533a1f0c1769771558763454b69188ceafa
minnnseokk <msjeong991029@gmail.com>
2025-05-07 16:26:25 +0900

    feature | sprint1 | FRB-127 | Equip 등록을 위한 존 매핑 설정 추가 | 정민석

--- src/main/java/com/factoreal/backend/repository/SensorRepository.java
+++ src/main/java/com/factoreal/backend/repository/SensorRepository.java
@@ -10,7 +10,4 @@ import java.util.List;
 
 public interface SensorRepository extends JpaRepository<Sensor, String> {
     Optional<Sensor> findBySensorId(String sensorId);
-
-    // 미등록 센서 리스트 조회 ( BE -> FE )
-    List<Sensor> findByRegisteredFalseAndSensorIdIsNotNull();
 }
