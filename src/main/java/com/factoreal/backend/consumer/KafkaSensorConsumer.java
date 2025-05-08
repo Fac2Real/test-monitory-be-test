fe3f3a15973bf5d21885a56cb47b80082d91b3b0
JuneYoo <juneyo0823@naver.com>
2025-05-08 10:14:12 +0900

    feat|sprint1|FRB-69| ELK 실시간 대시보드 |유승희g

--- src/main/java/com/factoreal/backend/consumer/KafkaSensorConsumer.java
+++ src/main/java/com/factoreal/backend/consumer/KafkaSensorConsumer.java
@@ -2,19 +2,27 @@ package com.factoreal.backend.consumer;
 
 import com.factoreal.backend.dto.SensorKafkaDto;
 import com.factoreal.backend.sender.WebSocketSender;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Map;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class KafkaSensorConsumer {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    private final WebSocketSender webSocketSender;
+    private final ObjectMapper objectMapper = new ObjectMapper(); // 데이터 파싱
+    private final WebSocketSender webSocketSender; // 웹소켓
+    private final RestHighLevelClient elasticsearchClient; // ELK client
 
 
     @KafkaListener(topics = {"EQUIPMENT", "ENVIRONMENT"}, groupId = "monitory-consumer-group")
@@ -25,6 +33,15 @@ public class KafkaSensorConsumer {
         try {
             SensorKafkaDto dto = objectMapper.readValue(message, SensorKafkaDto.class);
 
+
+            // ✅ Elasticsearch 저장
+            Map<String, Object> map = objectMapper.convertValue(dto, new TypeReference<>() {});
+            map.put("timestamp", Instant.now().toString());  //  현재 UTC 시간 ISO 8601 형식 문자열
+            IndexRequest request = new IndexRequest("sensor-data")
+                    .source(map);
+            elasticsearchClient.index(request, RequestOptions.DEFAULT);
+
+            // ⚠️ 위험 감지 및 웹소켓 전송
             // equipId가 비어있고 zoneId는 존재할 때만 처리
             if ((dto.getEquipId() == null || dto.getEquipId().isEmpty()) && dto.getZoneId() != null) {
 
